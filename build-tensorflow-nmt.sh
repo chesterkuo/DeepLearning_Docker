c5ef6b90b015bbbb20e9ba8df6f24f435e89a3c2
chester.kuo <chester.kuo@intel.com>
2017-09-11 00:13:19 +0800

    Tensorflow NMT example with Docker image

---
+++ build-tensorflow-nmt.sh
@@ -0,0 +1,1 @@
+sudo -E docker build --build-arg HTTP_PROXY=http://proxy-chain.intel.com:911 -t tensorflow-nmt-example:1.0 -f ./Dockerfile-x86-cuda8.0-cudnn6-ubuntu16.04 .
---
+++ run-nvidia-docker.sh
@@ -0,0 +1,1 @@
+sudo nvidia-docker run -it tensorflow-nmt-example:1.0 /bin/bash
