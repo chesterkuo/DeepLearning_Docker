b15429e92e7af5e19cfd57e2c1b50db9085ee75a
chester.kuo <chester.kuo@intel.com>
2017-09-12 11:31:34 +0800

    update NMT attention mode

--- Tensorflow-nmt-Example/run-nvidia-docker.sh
+++ Tensorflow-nmt-Example/run-nvidia-docker.sh
@@ -1,2 +1,2 @@
-mkdir -p nmt-host-model
-sudo nvidia-docker run -v `pwd`/nmt-host-model:/tmp/tensorflow-build/tensorflow-nmt/nmt_model -it tensorflow-nmt-vi-en-translation:1.0 /bin/bash
+mkdir -p nmt-host-attention-model
+sudo nvidia-docker run -v `pwd`/nmt-host-attention-model:/tmp/tensorflow-build/tensorflow-nmt/nmt_attention_model -it tensorflow-nmt-vi-en-translation:1.0 /bin/bash
